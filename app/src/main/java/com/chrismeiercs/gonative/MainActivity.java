a753a15b74da318c60b8707b99482f2afa43b7f8
chrismeiercs <chris.g.meier@gmail.com>
2017-03-25 18:49:18 -0400

    Added ability for app to save photos to the phone. The thumbnail is now loaded from the saved image URI.

--- app/src/main/java/com/chrismeiercs/gonative/MainActivity.java
+++ app/src/main/java/com/chrismeiercs/gonative/MainActivity.java
@@ -2,10 +2,13 @@ package com.chrismeiercs.gonative;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,11 +16,18 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
+    private String mCurrentPhotoPath;
     private ImageView imageThumbnail;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -61,22 +71,62 @@ public class MainActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
-    private void dispatchTakePictureIntent(){
+    private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null){
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                Toast.makeText(this, "etner", Toast.LENGTH_LONG).show();
+                photoFile = createImageFile();
+                Toast.makeText(this, "created", Toast.LENGTH_LONG).show();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Toast.makeText(this, "io error", Toast.LENGTH_LONG).show();
+            }
+            // Continue only if the File was successfully created
+            Toast.makeText(this, "continue", Toast.LENGTH_LONG).show();
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                Toast.makeText(this, "Store", Toast.LENGTH_LONG).show();
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                Toast.makeText(this, "intent", Toast.LENGTH_LONG).show();
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
         }
-
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageThumbnail.setImageBitmap(imageBitmap);
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            File image = new File(mCurrentPhotoPath);
+            imageThumbnail.setImageURI(Uri.fromFile(image));
+            Toast.makeText(this, "returned " + Uri.fromFile(image) , Toast.LENGTH_LONG).show();
+            /*if (data != null) {
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                imageThumbnail.setImageBitmap(imageBitmap);
+            } else {
+                Toast.makeText(this, "data is null", Toast.LENGTH_LONG).show();
+            }*/
+
         }
     }
 
+    private File createImageFile() throws IOException {
+        //Create image file name to prevent name collisions
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+
+        //save photo
+        mCurrentPhotoPath = image.getAbsolutePath();
+        Toast.makeText(this, mCurrentPhotoPath, Toast.LENGTH_LONG).show();
+        return image;
+    }
 
 }
