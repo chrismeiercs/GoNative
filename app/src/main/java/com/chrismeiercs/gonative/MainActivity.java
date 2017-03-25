854531a05ba7661e97704d02967243feb37bbed6
chrismeiercs <chris.g.meier@gmail.com>
2017-03-25 17:19:18 -0400

    Fixed UI by placing a constraint on the camera button. Added a listener to start camera when the camera button is pushed.

--- app/src/main/java/com/chrismeiercs/gonative/MainActivity.java
+++ app/src/main/java/com/chrismeiercs/gonative/MainActivity.java
@@ -1,15 +1,21 @@
 package com.chrismeiercs.gonative;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -37,9 +43,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageButton cameraButton = (ImageButton) findViewById(R.id.cameraButton);
+
+        cameraButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                dispatchTakePictureIntent();
+            }
+        });
+
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
+    private void dispatchTakePictureIntent(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+
+    }
+
+
 
 }
